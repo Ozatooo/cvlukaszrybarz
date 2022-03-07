441ff0b23c6b7199fd9074f83cc5236cc9b40994
Ozatooo <lukasz4044@o2.pl>
2022-03-07 22:34:37 +0100

    v1.0.19

--- script.js
+++ script.js
@@ -10,15 +10,15 @@ if (isMobile) {
 
 x_menu.addEventListener("click", function (e) {
 
-    mobile_menu[0].style.right = '0';
+    mobile_menu[0].style.right = '0%';
     mobile_menu[0].style.animation="shift 0.7s 1";
 
 });
 
 document.addEventListener('click', function(e) {
     var isClickInsideElement = x_menu.contains(event.target);
-    if (!isClickInsideElement) {
-        
+    if (!isClickInsideElement &&mobile_menu[0].style.right == '0%') {
+
         mobile_menu[0].style.animation="shift_b 0.5s 1";
         mobile_menu[0].style.right = '-110%';
     }
