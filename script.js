e1519da28d58c4f579e9bc921a7e1e6605d7599a
Ozatooo <61974211+Ozatooo@users.noreply.github.com>
2022-03-23 13:38:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// const isMobile = window.matchMedia("only screen and (max-width: 1200px)").matches;
-// const x_menu = document.getElementById("x_menu");
-// const mobile_menu = document.getElementsByClassName("mobile_menu");
+const isMobile = window.matchMedia("only screen and (max-width: 1200px)").matches;
+const x_menu = document.getElementById("x_menu");
+const mobile_menu = document.getElementsByClassName("mobile_menu");
  
 // if (isMobile) {
 //     document.getElementsByClassName("menu")[0].style.display = 'none';
@@ -8,18 +8,18 @@
 //     document.getElementsByClassName("x_menu")[0].style.display = 'none';
 // }
 
-// x_menu.addEventListener("click", function (e) {
+x_menu.addEventListener("click", function (e) {
 
-//     mobile_menu[0].style.right = '0%';
-//     mobile_menu[0].style.animation="shift 0.7s 1";
+    mobile_menu[0].style.right = '0%';
+    mobile_menu[0].style.animation="shift 0.7s 1";
 
-// });
+});
 
-// document.addEventListener('click', function(e) {
-//     var isClickInsideElement = x_menu.contains(event.target);
-//     if (!isClickInsideElement &&mobile_menu[0].style.right == '0%') {
+document.addEventListener('click', function(e) {
+    var isClickInsideElement = x_menu.contains(event.target);
+    if (!isClickInsideElement &&mobile_menu[0].style.right == '0%') {
 
-//         mobile_menu[0].style.animation="shift_b 0.5s 1";
-//         mobile_menu[0].style.right = '-110%';
-//     }
-// });
+        mobile_menu[0].style.animation="shift_b 0.5s 1";
+        mobile_menu[0].style.right = '-110%';
+    }
+});
