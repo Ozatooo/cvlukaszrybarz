4c3a307df6337e2edf8e1cc65ee569957966310b
Ozatooo <61974211+Ozatooo@users.noreply.github.com>
2022-04-19 23:45:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ hamburger.addEventListener("click", function (e) {
 });
 
 document.addEventListener('click', function(e) {
-    var isClickInsideElement = hamburger.contains(event.target);
+    const isClickInsideElement = hamburger.contains(event.target);
     if (!isClickInsideElement && mobile_menu[0].style.right == '0%') {
 
         mobile_menu[0].style.animation="shift_b 0.5s 1";
