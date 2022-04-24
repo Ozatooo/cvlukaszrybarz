82d1a24e4d431be9fe20d643dc275f9ea9209097
Ozatooo <lukasz4044@o2.pl>
2022-04-25 01:19:24 +0200

    nothing special

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const hamburger = document.getElementById("hamburger");
 const mobile_menu = document.getElementsByClassName("mobile_menu");
 
-hamburger.addEventListener("click", function (e) {
+hamburger.addEventListener("click", () => {
 
     mobile_menu[0].style.right = '0%';
     mobile_menu[0].style.animation="shift 0.7s 1";
 
 });
 
-document.addEventListener('click', function(e) {
+document.addEventListener('click', () => {
     const isClickInsideElement = hamburger.contains(event.target);
     if (!isClickInsideElement && mobile_menu[0].style.right == '0%') {
 
